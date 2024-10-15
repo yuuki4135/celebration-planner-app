81df72b7639588957f0bc959fa8ea2e58d9c5294
sugimoto.yuki <yuuki4135@gmail.com>
2024-10-15 23:04:42 +0900

    [add]chakraUI導入

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import * as React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
 )
