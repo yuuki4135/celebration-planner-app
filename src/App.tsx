d51bbf6539b792ca2416d460603c4a47e8db0350
sugimoto.yuki <yuuki4135@gmail.com>
2024-10-16 21:58:11 +0900

    [add]react-rooter-dom導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react'
+import { Top } from './components/pages/top'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { Profile } from './components/pages/profile';
+import { Login } from './components/pages/login';
+import Navbar  from './components/pages/templates/navbar';
 
-function App() {
+export const App = () => {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/login">Login</Link>
+        <Routes>
+          <Route path="/" element={<Top/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/pages/top.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Top = () => {
+  return (
+    <div>
+      <h1>Top Page</h1>
+    </div>
+  )
+}
