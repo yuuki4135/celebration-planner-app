73e27e39a2afaea19a8c68b22cf58d6338132ab8
sugimoto.yuki <yuuki4135@gmail.com>
2024-10-16 23:09:05 +0900

    [add]cloudfunctions追加

---
+++ functions/src/index.ts
@@ -0,0 +1,50 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+require('dotenv').config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+async function run() {
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash",
+    generation_config: {"response_mime_type": "application/json"}
+  });
+
+  const prompt = `次のJSONスキーマを使用して、
+                  人気のあるクッキーレシピを日本語で5個リストアップしてください。
+
+                  Recipe = {'recipe_name': str}
+                  Return: list[Recipe`;
+
+  const result = await model.generateContentStream(prompt);
+
+  let text = '';
+  for await (const chunk of result.stream) {
+    //const chunkText = chunk.text();
+    const chunkText = chunk.text();
+    console.log(chunkText);
+    text += chunkText;
+  }
+}
+
+run();
