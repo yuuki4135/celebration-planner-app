cc2f0c70ab5b0c7e65b5911fe2aae910a573db22
杉本 祐貴 <sugimoto.yuki@k-idea.jp>
2024-10-31 17:43:54 +0900

    [fix]不要なコンポーネント削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react'
 import { Top } from './components/pages/top'
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
-import { Profile } from './components/pages/profile';
-import { Login } from './components/pages/login';
-import Navbar  from './components/templates/navbar';
 
 export const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/login">Login</Link>
         <Routes>
           <Route path="/" element={<Top/>} />
-          <Route path="/profile" element={<Profile/>} />
-          <Route path="/login" element={<Login/>} />
           <Route path="*" element={<div>Not Found</div>} />
         </Routes>
       </BrowserRouter>
