e7a162a15a67eecf2ccc9cfc0adbdf1f1a143d53
杉本 祐貴 <sugimoto.yuki@k-idea.jp>
2024-11-22 15:59:25 +0900

    [fix]analiticsの設定変更

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_APP_KEY,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VITE_FIREBASE_STORAGEBUCKET,
